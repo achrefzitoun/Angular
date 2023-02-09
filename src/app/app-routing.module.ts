e2fa9ad1297b003a9456114d8037702e98fc4e58
Achref Zitoun <achref.zitoun@esprit.tn>
2023-02-09 21:08:25 +0100

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailProductsComponent } from './detail-products/detail-products.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { OffresEmploiComponent } from './offres-emploi/offres-emploi.component';
+import { ProductsComponent } from './products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'products',pathMatch:'full'},
+  {path:'products', component:ProductsComponent},
+  {path:'product/:id', component:DetailProductsComponent},
+  {path:'offres',component:OffresEmploiComponent},
+  {path:'**',component:NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { OffresEmploiComponent } from './offres-emploi/offres-emploi.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailProductsComponent } from './detail-products/detail-products.component';
+import { ArticlesComponent } from './articles/articles.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    NavbarComponent,
+    OffresEmploiComponent,
+    NotFoundComponent,
+    DetailProductsComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
